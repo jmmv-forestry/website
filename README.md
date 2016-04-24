ed413795d1055f32ab056fd25f7296eee3f9d1b4
Julio Merino <julio@meroh.net>
2016-04-24 17:54:14 -0400

    Add ability to publish to an external repository
    With the revamped site, I will make the Jekyll contents private and
    will start pushing out only the built site.  To make this easier,
    add a publish-site.sh script that does the publication work and a
    corresponding "publish" make target.

--- README.md
+++ README.md
@@ -18,3 +18,12 @@ example) and includes all posts in the tree.
 The default `build` and `serve` targets build the site for the `dev`
 environment.  Use `build-prod` and `serve-prod` to build the site for
 the `prod` environment.
+
+## Publication
+
+Run `make publish` to build the `prod` version of the site and publish
+it in a Git repository.  The contents of the Git repository will be
+completely replaced by the built site (though history is not wiped).
+
+The live site used for publication can be set with the `--live-repository`
+flag of the `configure` script.
---
+++ _admin/publish-site.sh
@@ -0,0 +1,40 @@
+# Publishes the built site on a separate Git repository.
+
+shtk_import cleanup
+shtk_import cli
+
+main() {
+    [ ${#} -eq 2 ] || shtk_cli_usage_error "Must provide the location of the" \
+        "built site and the target Git repository"
+    local site="${1}"; shift
+    local repo="${1}"; shift
+
+    [ -f "${site}/CNAME" ] || shtk_cli_error "Invalid site provided; CNAME" \
+        "not found in it"
+
+    local pattern="${TMPDIR:-/tmp}/$(shtk_cli_progname).XXXXXX"
+    local tempdir="$(mktemp -d "${pattern}" 2>/dev/null)"
+    [ -d "${tempdir}" ] || shtk_cli_error "Failed to create temporary directory"
+    eval "remove_tempdir() { rm -rf '${tempdir}'; }"
+    shtk_cleanup_register remove_tempdir
+
+    git clone "${repo}" "${tempdir}"
+    [ -f "${tempdir}/CNAME" ] || shtk_cli_error "Invalid repository provided;" \
+        "CNAME not found in it"
+    cmp -s "${site}/CNAME" "${tempdir}/CNAME" || shtk_cli_error "CNAMEs do" \
+        "not match; refusing to push"
+
+    rsync -av --delete-after --exclude=".git" "${site}/" "${tempdir}"
+
+    cd "${tempdir}"
+    for f in $(git status --porcelain | grep '^\?' | cut -c 4-); do
+        git add "${f}"
+    done
+    for f in $(git status --porcelain | grep '^ D' | cut -c 4-); do
+        git rm "${f}"
+    done
+
+    git status
+    git commit -a -m 'Publish rebuilt new site'
+    git push
+}
